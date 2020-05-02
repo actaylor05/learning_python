ee3f093a0861864ab308bea4ed1db49bc0948647
actaylor05 <actaylor@DESKTOP-GS9O07B.localdomain>
2020-05-01 20:16:45 -0700

    initial upload

---
+++ Week_5/rand_fasta.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import gzip
+import sys
+import math
+import random
+
+# Write a program that creates random fasta files
+# Create a function that makes random DNA sequences
+# Parameters include length and frequencies for A, C, G, T
+# Command line: python3 rand_fasta.py <count> <min> <max> <a> <c> <g> <t>
+
+def rand_dna(lenseq, a, c, g, t):
+    dnaseq = []
+    for i in range(lenseq):
+        r = random.random()
+        if r < a: 
+            dnaseq.append('A')
+        elif r < a+c:
+            dnaseq.append('C')
+        elif r < a+c+g:
+            dnaseq.append('G')
+        else:
+            dnaseq.append('T')
+    return ''.join(dnaseq)
+    
+count = int(sys.argv[1])
+
+min = int(sys.argv[2])
+max = int(sys.argv[3])
+a = float(sys.argv[4])
+c = float(sys.argv[5])
+g = float(sys.argv[6])
+t = float(sys.argv[7])
+
+for i in range(count):
+    x = random.randint(min, max)
+    dna = rand_dna(x, a, c, g, t)
+    print(f'>{i}')
+    print(dna)
+
+"""
+python3 rand_fasta.py 3 5 10 .3 .3 .2 .2
+>0
+GAGTAGAC
+>1
+GGTATGC
+>2
+CAACTGTCT
+"""
